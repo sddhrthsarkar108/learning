5accc993ecc22d88737c3afb75ce2a2d452d039d
Siddhartha Sarkar <sddhrthsarkar108@gmail.com.com>
2018-12-11 09:13:24 +0530

    initial commit linux

--- js/es6/rxjx/switchmap.js
+++
@@ -1,8 +0,0 @@
-var rx = require('rxjs/Rx');
-
-var obs1$ = rx.Observable.interval(1000);
-
-//obs1.subscribe(value => console.log(value));
-obs1$
-.map(value => value * 2)
-.subscribe(value => console.log(value));
