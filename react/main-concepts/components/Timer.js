72cdb6c5fe69383d44bbb34ff335f8cdfa995cdc
Siddhartha Sarkar <sddhrthsarkar108@gmail.com>
2019-04-24 16:54:23 +0530

    react jsx, elements, components, state

---
+++ react/main-concepts/components/Timer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => (
+    <h3>current time: {props.time}</h3>
+);
---
+++ react/main-concepts/webpack/webpack.dev.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+	entry: [
+		path.join(__dirname, '../index.js')
+    ],
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output: {
+        path: parentDir,
+        filename: 'bundle.js'
+    }
+}
